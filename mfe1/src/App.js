06b093ffb7d06a6f2a1d844c99ce38cc318c8bca
Harikrishnan Nair <Harikrishnan.Nair@neudesic.com>
2022-06-08 19:17:35 +0530

    first commit

---
+++ mfe1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import Button from './Button'; 
+
+const App = () => {
+  return (
+    <div>
+      <p style={{marginBottom: '10px'}}>Click below</p>
+      <Button />
+    </div>
+  );
+}
+
+export default App
---
+++ mfe1/src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+const button = () => {
+  return (
+    <div>
+        <button>
+            Button from MFE-1
+        </button>
+    </div>
+  )
+}
+
+export default button
